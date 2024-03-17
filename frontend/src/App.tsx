caac3af035941fdb5d8dbfc73da713a7ad8cbfee
subhajitdas1999 <sd651846@gmail.com>
2024-03-18 00:24:43 +0530

    added the initial home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
+import Home from "./components/Home";
 import { Test } from "./components/Test";
 import { Web3Provider } from "./components/Web3Provider";
 
@@ -8,7 +10,15 @@ function App() {
       <div className="bg-gray-800 min-h-screen text-white font-mono">
         <Web3Provider>
           <Header />
-          <Test />
+          {/* <Test /> */}
+          {/* <Home /> */}
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              {/* <Route path="about" element={<AboutPage />} /> */}
+              <Route path="/league/:id" element={<Test />} />
+            </Routes>
+          </BrowserRouter>
           {/* Rest of your app content */}
         </Web3Provider>
       </div>
