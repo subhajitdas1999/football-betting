4eaf4f4df7bda3ca0ab45a66a124e7bb6a3797d3
subhajitdas1999 <sd651846@gmail.com>
2024-03-21 01:03:31 +0530

    deleted prisma

--- backend/src/routes/dataSource.routes.ts
+++
@@ -1,7 +0,0 @@
-import { getNextFootballMatches } from "@controllers/dataSource.controller";
-import express from "express";
-
-const dataSourceRouter = express.Router();
-
-dataSourceRouter.get("/get-football-matches", getNextFootballMatches);
-export default dataSourceRouter;
