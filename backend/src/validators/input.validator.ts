f5a918405610065c172b7570d4bcead5d38446d1
subhajitdas1999 <sd651846@gmail.com>
2024-03-21 01:04:28 +0530

    added admin inputs validators

--- backend/src/validators/input.validator.ts
+++ backend/src/validators/input.validator.ts
@@ -3,18 +3,10 @@ import { z } from "zod";
 export const addNewLeagueInput = z.object({
   name: z.string(),
   logo: z.string(),
-  season: z
-    .string()
-    .transform((val) =>
-      val !== undefined && !isNaN(parseInt(val)) ? parseInt(val) : undefined
-    ),
+  season: z.string().transform((val) => parseInt(val)),
   start: z.string(),
   end: z.string(),
-  leagueId: z
-    .string()
-    .transform((val) =>
-      val !== undefined && !isNaN(parseInt(val)) ? parseInt(val) : undefined
-    ),
+  leagueId: z.string().transform((val) => parseInt(val)),
 });
 
 export const searchNoteQuery = z.object({
