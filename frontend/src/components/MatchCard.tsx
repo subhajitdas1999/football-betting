641bc53050ac772378921b0f2eeda3b3e8a24815
subhajitdas1999 <sd651846@gmail.com>
2024-03-25 01:46:00 +0530

    updated the matchCard

--- frontend/src/components/MatchCard.tsx
+++ frontend/src/components/MatchCard.tsx
@@ -1,55 +1,83 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
 import { Match } from "../interfaces";
+import { formatDateTime } from "../utils";
+import { PredictionProgressBar } from "./PredictionProgressBar";
+import { PredictModal } from "./PredictModal";
 
 interface Props {
   data: Match;
 }
-function formatDateTime(dateTimeString: string) {
-  const date = new Date(dateTimeString);
-  const options: Intl.DateTimeFormatOptions = {
-    day: "2-digit", // '2-digit' or 'numeric'
-    month: "short", // 'numeric', '2-digit', 'narrow', 'short', or 'long'
-    year: "numeric", // '2-digit' or 'numeric'
-    hour: "2-digit", // '2-digit' or 'numeric'
-    minute: "2-digit", // '2-digit' or 'numeric'
-    hour12: false,
-  };
-
-  // Split the date and time to remove the comma after the year
-  const formattedDate = date.toLocaleDateString("en-US", options);
-
-  return formattedDate;
-}
 
 export const MatchCard: React.FC<Props> = ({ data }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handlePredictClick = () => {
+    setIsModalOpen(true); // Open the modal
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false); // Close the modal
+  };
+  const handleTeam1Click = () => {
+    console.log(`You clicked ${data.teams.home.name}`);
+    // You can add additional logic here
+  };
+
+  const handleTeam2Click = () => {
+    console.log(`You clicked ${data.teams.away.name}`);
+    // You can add additional logic here
+  };
+
   return (
-    <Link to={`/league/${data.fixture.id}-${data.fixture.date}`}>
-      <div className="p-4 rounded-lg border border-gray-300 transform transition-all hover:scale-105 max-w-xl mx-auto">
-        <div className="flex flex-col items-center">
-          <div className="flex items-center space-x-4">
-            <div className="flex flex-col items-center">
-              <img
-                alt={data.teams.home.name}
-                className="w-20 h-20 rounded-full bg-gray-100 object-cover"
-                src={data.teams.home.logo}
-              />
-              <h2 className="text-xl font-bold mt-2">{data.teams.home.name}</h2>
-            </div>
-            <span className="text-2xl font-bold">vs</span>
-            <div className="flex flex-col items-center">
-              <img
-                alt={data.teams.away.name}
-                className="w-20 h-20 rounded-full bg-gray-100 object-cover"
-                src={data.teams.away.logo}
-              />
-              <h2 className="text-xl font-bold mt-2">{data.teams.away.name}</h2>
-            </div>
+    <div className="p-4 rounded-lg border border-gray-300 transform transition-all hover:scale-105 max-w-xl mx-auto w-full">
+      <div className="flex flex-col items-center">
+        <div className="flex items-center space-x-4">
+          <div className="flex flex-col items-center">
+            <img
+              alt={data.teams.home.name}
+              className="w-20 h-20 rounded-full bg-gray-100 object-cover"
+              src={data.teams.home.logo}
+            />
+            <h2 className="text-xl font-bold mt-2 min-w-[150px]">
+              {data.teams.home.name}
+            </h2>
           </div>
-          <p className="text-gray-400 mt-4">
-            {formatDateTime(data.fixture.date)}
-          </p>
+          <span className="text-2xl font-bold">vs</span>
+          <div className="flex flex-col items-center">
+            <img
+              alt={data.teams.away.name}
+              className="w-20 h-20 rounded-full bg-gray-100 object-cover"
+              src={data.teams.away.logo}
+            />
+            <h2 className="text-xl font-bold mt-2 min-w-[150px]">
+              {data.teams.away.name}
+            </h2>
+          </div>
         </div>
+        <p className="text-gray-400 mt-4 h-6">
+          {formatDateTime(data.fixture.date)}
+        </p>
+        <div className="my-4 flex justify-center align-middle w-full">
+          <div className="">
+            <button
+              className="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded"
+              onClick={handlePredictClick}
+            >
+              Predict
+            </button>
+          </div>
+          <div className="py-2 px-4">Amount of Matic:12.044</div>
+        </div>
+        <PredictionProgressBar homeCount={10} awayCount={36} />
       </div>
-    </Link>
+      <PredictModal
+        isOpen={isModalOpen}
+        onClose={handleCloseModal}
+        team1={data.teams.home.name}
+        team2={data.teams.away.name}
+        onTeam1Click={handleTeam1Click}
+        onTeam2Click={handleTeam2Click}
+      />
+    </div>
   );
 };
