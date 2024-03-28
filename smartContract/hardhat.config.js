a4fd65e815b673308fa65e1a06078de899597620
subhajitdas1999 <sd651846@gmail.com>
2024-03-29 01:57:54 +0530

    added networks

--- smartContract/hardhat.config.js
+++ smartContract/hardhat.config.js
@@ -1,6 +1,32 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("@openzeppelin/hardhat-upgrades");
+require("@nomicfoundation/hardhat-verify");
+require("dotenv").config({ path: "./.env" });
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.21",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    mumbai: {
+      url: process.env.MUMBAI_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      polygonMumbai: process.env.POLYGON_API_KEY,
+    },
+  },
 };
